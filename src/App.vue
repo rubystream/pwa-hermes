f03bfd981292fc6b4d7ae7cab7953555cdfdbe74
Dejan Dimic <dejan.dimic@gmail.com>
2017-12-17 22:40:29 +0100

    added bootstrap-vue

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,14 @@
 </template>
 
 <script>
+import Vue from 'vue'
+import BootstrapVue from 'bootstrap-vue'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(BootstrapVue)
+
 export default {
   name: 'app'
 }
